f2165dfb64ab1f8377426b4e958364df71f8138e
pooh <cktjdgus45@naver.com>
2022-04-08 15:56:51 +0900

    quote game js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+
+let startTime;
+
+function startTimer() {
+    timerElement.innerText = 0;
+    startTime = new Date();
+    setInterval(() => {
+        timerElement.innerText = getTimerTime();
+    }, 1000);
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000);
+}
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
@@ -8,20 +23,39 @@ function getRandomQuote() {
         .then(data => data.content);
 }
 
-async function getNextQuote() {
+async function renderNewQuote() {
     const quote = await getRandomQuote();
     quoteDisplayElement.innerHTML = '';
-    console.log(quote.split(''));
     quote.split('').forEach(char => {
         const span = document.createElement('span');
         span.innerText = char;
         quoteDisplayElement.appendChild(span);
     })
     quoteInputElement.value = null;
+    startTimer();
 }
 
-getNextQuote();
+renderNewQuote();
 
 quoteInputElement.addEventListener('input', () => {
-    console.log('changed');
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        }
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        }
+    })
+    if (correct) renderNewQuote();
 })
