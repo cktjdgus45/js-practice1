72bcc11b49a7b8318426d6f42f1e4afcb0f27d52
pooh <cktjdgus45@naver.com>
2022-04-19 14:44:35 +0900

    Iterable JS

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-const item1 = { name: 'milk1', price: 3 };
-const item2 = { name: 'milk2', price: 4 };
-const item3 = { name: 'milk3', price: 5 };
+const multiple = {
+    [Symbol.iterator]: () => {
+        let num = 0;
+        const max = 10;
+        return {
+            next() {
+                return { value: num++ * 2, done: num > max }
+            }
+        }
+    }
+}
 
-const products = [item1, item2, item3];
-
-let found = products.find(value => value.name = "milk2");
-console.log(found)
-
-found = products.some(value => value.name === "milk2");
-console.log(found)
-
-found = products.every(value => value.name === "milk2");
-console.log(found)
-
-found = products.filter(value => value.price === 2);
-console.log(found)
-
-found = products.map(value => value.price * 2);
-console.log(found)
-
-let result = [1, 2, 3, 4, 5].reduce((sum, value) => {
-    sum += value;
-    return sum;
-})
-
-console.log(result)
+for (const num of multiple) {
+    console.log(num)
+}
