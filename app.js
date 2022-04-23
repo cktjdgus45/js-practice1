ad99579fd0f801e01b02f195348bc39d9e002ecf
pooh <cktjdgus45@naver.com>
2022-04-23 21:05:43 +0900

    dynamic binding JS

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
-//왜 클로져가 필요한것인지.
-//내부정보를 은닉하고 , 공개함수(public)를 통한 데이터 조작을 위해 사용한다.
-//캡슐화와 정보은닉
-//클래스 private 사용하는것과 똑같은 효과.
-//지금은 prvate있지만 옛날버전에선 없었기때문에 클로저특징을 통해 정보를 은닉함.
-
-function makeCounter() {
-    let count = 0;
-    function increase() {
-        count++;
-        console.log(count);
+function Cat(name) {
+    this.name = name;
+    this.printName = function () {
+        console.log(`고양이이름양옹:${this.name}`)
     }
-    return increase;
 }
-
-const increaseFunc = makeCounter();
-
-increaseFunc();
-increaseFunc();
-increaseFunc();
-
-//class로 대체가능
-
-class Counter {
-    #count = 0;
-    increase() {
-        this.#count++;
-        console.log(this.#count);
+function Dog(name) {
+    this.name = name;
+    this.printName = function () {
+        console.log(`강아지이름멍멍:${this.name}`)
     }
 }
 
-const counter = new Counter();
-counter.increase();
+
+const cat = new Cat('쑤티');
+const dog = new Dog('포포');
+
+dog.printName = cat.printName;
+dog.printName();
